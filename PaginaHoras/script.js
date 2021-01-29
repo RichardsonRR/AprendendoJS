7462fa47661f790eb916cc4c11844a5aa66cd088
Richardson Rodrigues Abreu <richardsonr@Richardsons-iMac.local>
2021-01-28 16:24:14 -0800

    add de minutos nas horas

--- PaginaHoras/script.js
+++ PaginaHoras/script.js
@@ -3,21 +3,22 @@ var msg = document.getElementById('msg')
 var imagem = document.getElementById('imgHora')
 var data = new Date ()
 var horas = data.getHours()
+var minutos =  data.getMinutes()
 
 
 
 if( horas >= 0 && horas < 12){
-    msg.innerHTML= `Bom Dia!  Agora sao ${horas} horas`
+    msg.innerHTML= `Bom Dia!  Agora sao ${horas}:${minutos} horas`
     imagem.src = 'imgs/pexels-reneereneee-3993720.png'
     document.body.style.background = '#d3be9f'
 
 }else if (horas >= 12 && horas < 18 ){
-    msg.innerHTML= `Boa tarde!  Agora sao ${horas} horas`
+    msg.innerHTML= `Boa tarde!  Agora sao ${horas}:${minutos} horas`
     imagem.src = 'imgs/Tarde.png'
     document.body.style.background = '#d9e0e3'
 
 }else {
-    msg.innerHTML= `Boa noite!  Agora sao ${horas} horas`
+    msg.innerHTML= `Boa noite!  Agora sao ${horas}:${minutos} horas`
     imagem.src = 'imgs/noite.png'
     document.body.style.background = '#09181b'
 
