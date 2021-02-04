4e5572f900745328a774d506d0dfae432d2cabd7
Richardson Rodrigues Abreu <richardsonr@Richardsons-iMac.local>
2021-02-03 22:34:53 -0300

    Cronometro Simples

---
+++ Cronometro/cronometro.js
@@ -0,0 +1,55 @@
+let campoHoras, horas, minutos,segundos; 
+compohoras = document.getElementById('msg');
+horas = 0;
+minutos = 0;
+segundos= 0; 
+let contador;
+ 
+
+function start(){
+    contador = setInterval(() => cronometro(),1000)
+    let botaoStart = document.getElementById('botaoStart');
+    botaoStart.style.display = 'none'; 
+    
+
+    
+
+    
+}
+function pause(){
+    clearInterval(contador)
+    botaoStart.style.display= 'block'; 
+    botaoStart.style.margin= 'auto'
+
+
+}
+function reset(){
+    clearInterval(contador)
+    horas= 0; minutos= 0; segundos= 0; 
+    msg.innerHTML = `${horas}0:${minutos}0:${segundos}0`
+    botaoStart.style.display= 'block'; 
+    botaoStart.style.margin= 'auto';
+
+     
+}
+
+
+
+function cronometro(){
+segundos ++; 
+if(segundos == 60){
+    segundos = 0;
+    minutos++; 
+
+
+    if(minutos == 60){
+        minutos= 0; 
+        horas++; 
+
+    }
+} 
+
+let formatoDeHoras = (horas<10 ? '0'+ horas: horas )+ ':'+(minutos<10 ? '0'+minutos: minutos)+':'+(segundos<10? '0'+segundos: segundos)
+msg.innerHTML = formatoDeHoras; 
+
+}
